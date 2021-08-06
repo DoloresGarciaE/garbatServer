9f3da333b3acc797d93d11a313fdce4df0919c17
Facundo Rodriguez <rodriguezfacundohernan@gmail.com>
2021-08-06 20:25:06 -0300

    Added cors

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require("express");
 const environmentVariables = require("./config/environmentConfig");
 const db = require("./models/index");
 const routes = require("./routes/index");
-
+const cors = require("cors");
 const app = express();
 
+app.use(cors());
 app.use(routes);
 
 db.connection(environmentVariables.DB.DB_URI)
